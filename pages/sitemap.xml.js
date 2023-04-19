0611ea6066714ba408e13981ff15257c5870de28
unknown <Prajjwal Rajput>
2023-04-19 23:35:15 +0530

    sitemap.xml

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,69 @@
+import { getGraphQLQuery } from "../lib/graphql";
+
+const Sitemap = () => {};
+
+export async function getServerSideProps({ res }) {
+    const limit = 100;
+    let hasNextPage = true;
+    let endCursor = null;
+    let posts = [];
+  
+    while (hasNextPage) {
+      const data = await getGraphQLQuery(`
+        query {
+          posts(
+            where: { status: PUBLISH }
+            first: ${limit}
+            after: "${endCursor || ''}"
+          ) {
+            nodes {
+              slug
+              modified
+            }
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+          }
+        }
+      `);
+  
+      posts = [...posts, ...data.posts.nodes];
+      hasNextPage = data.posts.pageInfo.hasNextPage;
+      endCursor = data.posts.pageInfo.endCursor;
+    }
+
+
+  let sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+  // Add blog post URLs to sitemap
+  posts.forEach((post) => {
+    sitemapXml += `
+      <url>
+        <loc>https://howtoshout.com/${post.slug}</loc>
+        <lastmod>${post.modified}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>`;
+  });
+
+  sitemapXml += `
+    </urlset>`;
+
+  // Set content-type header to XML
+  res.setHeader("Content-Type", "text/xml");
+
+  // Write the sitemap XML to the response
+  res.write(sitemapXml);
+
+  // End the response
+  res.end();
+
+  // Return an empty object as props
+  return {
+    props: {},
+  };
+}
+
+export default Sitemap;
