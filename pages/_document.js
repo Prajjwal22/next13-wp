a10b81a10156022c1147b991b9cb3df0009d122d
unknown <Prajjwal Rajput>
2023-05-10 00:15:08 +0530

    gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Partytown } from '@builder.io/partytown/react';
 
 export default function Document() {
   return (
     <Html>
       <Head>
+      <Partytown debug={true} forward={['dataLayer.push']} />
+      <script
+  type="text/partytown"
+  dangerouslySetInnerHTML={{
+    __html:`
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MX4F838');</script>
+    <!-- End Google Tag Manager -->`,
+  }}
+/>
       </Head>
       <body>
         <Main />
