f7ee2d8f35dcc9c6db84213fdfc1dac5900b8285
prajjwal <howtoshout@gmail.com>
2024-06-15 20:56:47 +0530

    adsense

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import Script from "next/script";
+import React from "react";
 
 export default function PostLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <>{children}</>
-  )
+    <>
+      {" "}
+      <Script
+        strategy="worker"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LFFTL0Y202"
+      />
+      <Script
+        id="adsbygoogle-init"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5253670042601731"
+      />
+      {children}
+    </>
+  );
 }
